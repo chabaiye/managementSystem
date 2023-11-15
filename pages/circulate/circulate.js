df13c1e159c068ccbcb8d5a838b359463acdddcc
chabaiye <1493265049@qq.com>
2023-11-15 10:32:16 +0800

    7

--- pages/circulate/circulate.js
+++ pages/circulate/circulate.js
@@ -5,6 +5,7 @@ Page({
    * 页面的初始数据
    */
   data: {
+    tabCur:0,
     activeNames: ['1'],
     goods: [{
       "name":"工具",
@@ -34,11 +35,26 @@ Page({
           "num":80
         }]
       }]
-    }]
+    }],
+    category:{
+      "tools":["钳子类","螺刀类","扳手类","锤子类","专用工具类"],
+      "measuring":["机械类","电气类","电子类"],
+      "largeEquipment":["飞机","地面设备"],
+      "aviation":["机械系统类","电气系统类","电子系统类"],
+      "consumables":["航材类","民品类"],
+      "dangerous":["易燃易爆类"]    
+    }
   },
    onChange(event) {
     this.setData({
       activeNames: event.detail,
     })
+  },
+  onCategory(e){
+    console.log("index:"+e.currentTarget.dataset.id);
+    this.setData({
+      'tabCur':e.currentTarget.dataset.id
+    })
+    console.log("tabCur:"+this.data.tabCur);
   }
 })
